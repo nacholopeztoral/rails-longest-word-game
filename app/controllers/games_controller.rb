d979394ecd02d988308a7e22abb43da9bda826ef
nacho lopez <nachete-1995@hotmail.com>
2018-11-12 17:23:14 +0100

    finoshed second exercise

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,29 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
+  $array = ('a'..'z').to_a
   def new
+    # @array = ("a".."z").to_a
+    @array = $array
   end
 
   def score
+    word = params[:word]
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read.to_s)
+    final_word = word.split(//)
+    if json['found'] == false
+      @message = 'Not a word in the dictionary'
+    else
+      final_word.each do |x|
+        if final_word.count(x) == $array.count(x)
+        @message = 'Valid word'
+        @count = json['length']
+        else
+        @message = 'Invalid'
+        end
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'games/new'
-  get 'games/score'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'games/new', to: 'games#new', as: :new
+
+  post 'games/score', to: 'games#score', as: :score
+
+  get 'games/score', to: 'games#score', as: :scores
 end
